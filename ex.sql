ca617310b382c01c16837bf37ee1808f83e28203
Sho Yano <sho.yano@mac001-est-tln.lan>
2018-09-25 21:22:13 +0300

    ex4

--- ex.sql
+++ ex.sql
@@ -9,3 +9,9 @@ SELECT DISTINCT open_emp_id FROM account;
 
 -- 3-4
 SELECT p.product_cd, a.cust_id, a.avail_balance FROM product p INNER JOIN account a ON p.product_cd = a.product_cd WHERE p.product_type_cd = 'ACCOUNT' ORDER BY p.product_cd, a.cust_id;
+
+--4-3
+SELECT * FROM account WHERE open_date REGEXP '^2002';
+
+--4-4
+SELECT * FROM individual WHERE lname REGEXP '^.a' AND lname REGEXP 'a.*e';
