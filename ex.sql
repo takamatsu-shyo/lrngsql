061aa0bc069af0cef67cba505980b99bc67025dc
Sho Yano <sho.yano@mac001-est-tln.lan>
2018-09-27 22:49:14 +0300

    ex5

--- ex.sql
+++ ex.sql
@@ -15,3 +15,12 @@ SELECT * FROM account WHERE open_date REGEXP '^2002';
 
 --4-4
 SELECT * FROM individual WHERE lname REGEXP '^.a' AND lname REGEXP 'a.*e';
+
+--5-1
+SELECT e.emp_id, e.fname, e.lname, b.name FROM employee e INNER JOIN branch b ON e.assigned_branch_id = b.branch_id;
+
+--5-2
+SELECT a.account_id, c.fed_id, p.name FROM account a INNER JOIN customer c ON a.cust_id = c.cust_id INNER JOIN product p ON a.product_cd = p.product_cd WHERE c.cust_type_cd='I';
+
+--5-3
+SELECT e.emp_id , e.fname, e.lname FROM employee e INNER JOIN employee mgr ON e.superior_emp_id = mgr.emp_id WHERE e.dept_id != mgr.dept_id;
